73d959e175d3ad5ccbc9499637e20781980801fd
erasmo <erasmomontes07@gmail.com>
2019-05-04 08:40:48 -0400

    Add model Todo on todo app.

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Todo(models.Model):
+    
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("Todo_detail", kwargs={"pk": self.pk})
